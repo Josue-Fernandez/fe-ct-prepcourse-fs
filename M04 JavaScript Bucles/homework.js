68008687584896e99b9fcea8ac8cbc5fa05d4fd9
Josue-Fernandez <fernandezt.josue@gmail.com>
2023-02-13 22:35:00 -0400

    cambios en modulo 4, homework

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -4,7 +4,13 @@ function obtenerMayor(x, y) {
    // "x" e "y" son números enteros.
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
-   // Tu código:
+   if (x>y){
+      return x
+   }else if(y>x){
+      return y
+   }else if(y==x){
+      return x||y 
+   }
 }
 
 function mayoriaDeEdad(edad) {
